2c4ce625464bb0d19c8fb389261d2b684dadd965
Nick King <nrking0@gmail.com>
2021-03-31 16:27:44 -0500

    Project setup

---
+++ cmake-build-debug/CMakeFiles/naive-bayes-test.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/nrking0/Code/cinder_0.9.2_mac/my-projects/naive-bayes/src/core/rename_this_file.cc" "/Users/nrking0/Code/cinder_0.9.2_mac/my-projects/naive-bayes/cmake-build-debug/CMakeFiles/naive-bayes-test.dir/src/core/rename_this_file.cc.o"
+  "/Users/nrking0/Code/cinder_0.9.2_mac/my-projects/naive-bayes/src/visualizer/naive_bayes_app.cc" "/Users/nrking0/Code/cinder_0.9.2_mac/my-projects/naive-bayes/cmake-build-debug/CMakeFiles/naive-bayes-test.dir/src/visualizer/naive_bayes_app.cc.o"
+  "/Users/nrking0/Code/cinder_0.9.2_mac/my-projects/naive-bayes/src/visualizer/sketchpad.cc" "/Users/nrking0/Code/cinder_0.9.2_mac/my-projects/naive-bayes/cmake-build-debug/CMakeFiles/naive-bayes-test.dir/src/visualizer/sketchpad.cc.o"
+  "/Users/nrking0/Code/cinder_0.9.2_mac/my-projects/naive-bayes/tests/rename_this_test_file.cc" "/Users/nrking0/Code/cinder_0.9.2_mac/my-projects/naive-bayes/cmake-build-debug/CMakeFiles/naive-bayes-test.dir/tests/rename_this_test_file.cc.o"
+  "/Users/nrking0/Code/cinder_0.9.2_mac/my-projects/naive-bayes/tests/test_main.cc" "/Users/nrking0/Code/cinder_0.9.2_mac/my-projects/naive-bayes/cmake-build-debug/CMakeFiles/naive-bayes-test.dir/tests/test_main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "FT2_BUILD_LIBRARY"
+  "FT_DEBUG_LEVEL_TRACE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "_deps/catch2-src/single_include"
+  "/Users/nrking0/Code/cinder_0.9.2_mac/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/train-model.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/train-model.dir/apps/train_model_main.cc.o"
+  "CMakeFiles/train-model.dir/src/core/rename_this_file.cc.o"
+  "train-model"
+  "train-model.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/train-model.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
