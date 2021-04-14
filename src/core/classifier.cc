4023ceff16dd359e354bdc9a154395b0b33af3e5
Nick King <nrking0@gmail.com>
2021-04-14 18:57:19 -0500

    Added more documentation to classifier

--- src/core/classifier.cc
+++ src/core/classifier.cc
@@ -15,15 +15,18 @@ void Classifier::ClassifyImageSet(ImageProcessor &imageProcessor) {
         int max_index = -1;
 
         for (size_t i = 0; i < 10; i++) {
+            // Starts off with the overall probability of each class
             double probability = log(model_.GetProbClassC()[i]);
 
             for (size_t row = 0; row < image.GetSize(); row++) {
 
                 for (size_t col = 0; col < image.GetSize(); col++) {
+                    // Adds in each individual pixel probability based on conditions
                     probability += log(model_.GetProbPixelValues()[row][col][i][image.GetPixelShade(row, col)]);
                 }
             }
 
+            // Checking to see if the latest class is more likely than prior probabilities
             if (probability > max_probability) {
                 max_probability = probability;
                 max_index = i;
@@ -39,22 +42,26 @@ std::vector<double> Classifier::ClassifyImage(Image &image) {
     int max_index = -1;
 
     for (size_t i = 0; i < 10; i++) {
+        // Starts off with the overall probability of each class
         double probability = log(model_.GetProbClassC()[i]);
 
         for (size_t row = 0; row < image.GetSize(); row++) {
 
             for (size_t col = 0; col < image.GetSize(); col++) {
+                // Adds in each individual pixel probability based on conditions
                 probability += log(model_.GetProbPixelValues()[row][col][i][image.GetPixelShade(row, col)]);
             }
         }
 
         probabilities.push_back(probability);
+        // Checking to see if the latest class is more likely than prior probabilities
         if (probability > max_probability) {
             max_probability = probability;
             max_index = i;
         }
     }
     image.SetClassification(max_index);
+    // Returns vector of all likelihoods for testing purposes
     return probabilities;
 }
 
@@ -64,6 +71,8 @@ double Classifier::ValidateImageSetClassification(ImageProcessor &imageProcessor
 
     for (const Image& image : imageProcessor.GetImages()) {
 
+        // Simply iterates through each image and checks to see
+        // if it was correctly classified
         if (image.GetValue() == image.GetClassification()) {
             image_count++;
             correct_classification_count++;
